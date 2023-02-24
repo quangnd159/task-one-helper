fb7c45a864501ada2d77f5f9107153c16b9bff5f
Quang <quangnd159@gmail.com>
2023-02-24 19:01:52 +0700

    push the app

--- utils/helpers.tsx
+++ utils/helpers.tsx
@@ -9,7 +9,7 @@ export async function getOpenAIResponse(
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
     },
     body: JSON.stringify({
       model: "text-davinci-003",
