4649faa0385120831564226e0be72cf3010cfaa5
Quang <quangnd159@gmail.com>
2023-02-24 19:14:33 +0700

    push the app

--- utils/helpers.tsx
+++ utils/helpers.tsx
@@ -5,11 +5,12 @@ type OpenAIResponse = {
 export async function getOpenAIResponse(
   request: string
 ): Promise<string> {
+  console.log(process.env.OPENAI_API_KEY);
   const response = await fetch("https://api.openai.com/v1/completions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
     },
     body: JSON.stringify({
       model: "text-davinci-003",
