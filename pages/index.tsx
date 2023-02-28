3351006537f62b59bfabf6a6d0463fea079b02ff
Quang Nguyen Dang <quangnguyendang@Quangs-MacBook-Air.local>
2023-02-28 20:09:01 +0700

    add emoji favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,7 @@ export default function Home() {
     <div className="container mx-auto mt-10 px-4">
       <Head>
         <title>Task One Helper</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎩</text></svg>" />
       </Head>
 
       <main>
