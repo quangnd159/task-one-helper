40801e2572cdfe816330c3147321670f5d045e48
Quang <quangnd159@gmail.com>
2023-03-04 07:31:48 +0700

    added serverless function to hide api

---
+++ pages/api/openai.ts
@@ -0,0 +1,37 @@
+import { NextApiHandler } from "next";
+
+type OpenAIResponse = {
+  choices?: Array<{ message: { content: string } }>;
+};
+
+const handler: NextApiHandler = async (req, res) => {
+  const { request } = req.body;
+
+  const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { "role": "system", "content": "You are a professional writer who writes about charts, graphs, process diagrams and maps." },
+        { "role": "user", "content": `Use your IELTS Writing Task 1 knowledge to write three variations of sentence: ${request}. Wrap each sentence in <li></li>. Highlight idiomatic phrases by wrapping them with <em></em>. Do not include phrases like "according to the survey data" or "as revealed by the survey findings"` }
+      ],
+      temperature: 0.5,
+      // max_tokens: 200,
+      // top_p: 1,
+      // frequency_penalty: 0,
+      // presence_penalty: 0,
+    }),
+  });
+
+  const data: OpenAIResponse = await response.json();
+  const choices = data?.choices || [];
+  const content = choices.length > 0 ? choices[0].message.content : "";
+
+  res.status(200).json({ content });
+};
+
+export default handler;
--- utils/helpers.tsx
+++ utils/helpers.tsx
@@ -1,31 +1,17 @@
 type OpenAIResponse = {
-  choices?: Array<{ message: { content: string } }>;
+  content: string;
 };
 
 export async function getOpenAIResponse(request: string): Promise<string> {
-  const response = await fetch("https://api.openai.com/v1/chat/completions", {
+  const response = await fetch("/api/openai", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
     },
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo",
-      messages: [
-        { "role": "system", "content": "You are a professional writer who writes about charts, graphs, process diagrams and maps." },
-        { "role": "user", "content": `Use your IELTS Writing Task 1 knowledge to write three variations of sentence: ${request}. Wrap each sentence in <li></li>. Highlight idiomatic phrases by wrapping them with <em></em>. Do not include phrases like "according to the survey data" or "as revealed by the survey findings"` }
-      ],
-      temperature: 0.5,
-      // max_tokens: 200,
-      // top_p: 1,
-      // frequency_penalty: 0,
-      // presence_penalty: 0,
-    }),
+    body: JSON.stringify({ request }),
   });
 
   const data: OpenAIResponse = await response.json();
-  const choices = data?.choices || [];
-  const content = choices.length > 0 ? choices[0].message.content : "";
 
-  return content;
+  return data.content;
 }
